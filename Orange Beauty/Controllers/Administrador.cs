583919cc89d1e1648a55c32a8faffdf090eda307
Julia "jubsz <JuliaAmaralSantos@outlook.com>
2024-05-03 07:38:26 -0300

    feito pag login e link dos botoes pra pag adm e colab

---
+++ Orange Beauty/Controllers/Administrador.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Orange_Beauty.Controllers
+{
+    public class Administrador : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
